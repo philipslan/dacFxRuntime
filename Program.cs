6b25280c8647b10190cddb9282de02610733a2cd
Philip Lan <philipslan@github.com>
2020-10-05 16:17:51 -0400

    deleting stuff

--- Program.cs
+++ Program.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using System.Security;
+using Microsoft.SqlServer.Dac;
 using Microsoft.SqlServer.Dac.Compare;
 
 namespace dacFXProject
 {
   class Program
   {
+    // var dac = new DacServices("Data Source=mssql;Initial Catalog=TestDb;Integrated Security=False;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;User Id=sa;Password=SqlPassw0rd;");
+    // Extract Dacpac 
+    // ------------------
+    // dac.Extract("/Users/philiplan/code/github/dacFXProject/TestDb.dacpac", "TestDb", "TestDb", new Version(1, 0));
+    // 
+    // Deploy Dacpac
+    // ------------------
+    // DacPackage package = DacPackage.Load("/Users/philiplan/code/github/dacFXProject/TestDb.dacpac");
+    // dac.Deploy(package, "TESTDB1");
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
-      var pass = new SecureString();
-      var test = new SchemaCompareDatabaseEndpoint("Test", pass);
+        var src = new SchemaCompareDacpacEndpoint("./TestDb.dacpac");
+        var target = new SchemaCompareDacpacEndpoint("./TestDb.dacpac");
+
+        var schemaComparison = new SchemaComparison(src, target);
+
+        schemaComparison.Options.IgnorePermissions = true;
+        schemaComparison.Options.IgnoreRoleMembership = true;
+        var result = schemaComparison.Compare();
+        Console.WriteLine($"Result is equal: {result.IsEqual}, is valid: {result.IsValid}");
     }
   }
 }
