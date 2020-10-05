6889413e22fc1396134f6f82262a76eea94bc607
Philip Lan <philipslan@github.com>
2020-10-05 16:14:54 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security;
+using Microsoft.SqlServer.Dac.Compare;
+
+namespace dacFXProject
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      var pass = new SecureString();
+      var test = new SchemaCompareDatabaseEndpoint("Test", pass);
+    }
+  }
+}
